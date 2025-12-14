e7fc5f454453e9289152cdb8c97768e85f511348
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-14 13:04:23 +0530

    Update __init__.py

--- Nexa/database/__init__.py
+++ Nexa/database/__init__.py
@@ -1,1 +1,13 @@
-. 
+from .client import (
+    set_nsfw_status,
+    get_nsfw_status,
+    get_cached_scan,
+    cache_scan_result
+)
+
+__all__ = [
+    "set_nsfw_status",
+    "get_nsfw_status",
+    "get_cached_scan",
+    "cache_scan_result"
+]
