5bd48f4ed9248a688de70b5d63b87add5fb4d21f
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-15 16:43:39 +0530

    Update session.py

--- Nexa/core/session.py
+++ Nexa/core/session.py
@@ -1,1 +1,9 @@
-IVAN
+import aiohttp
+
+_session = None
+
+async def get_session():
+    global _session
+    if not _session or _session.closed:
+        _session = aiohttp.ClientSession()
+    return _session
