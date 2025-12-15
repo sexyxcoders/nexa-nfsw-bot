3ff58cdb89dc39d0e56a828d54b0b7030673149a
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-12-15 16:43:06 +0530

    Update redis.py

--- Nexa/database/redis.py
+++ Nexa/database/redis.py
@@ -1,1 +1,13 @@
-IVAN
+import os, redis, json
+
+redis_db = redis.from_url(
+    os.getenv("REDIS_URL"),
+    decode_responses=True
+)
+
+def redis_get(key):
+    v = redis_db.get(key)
+    return json.loads(v) if v else None
+
+def redis_set(key, value, ttl=3600):
+    redis_db.setex(key, ttl, json.dumps(value))
